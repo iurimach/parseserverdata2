b0ffb656287ad2678b0f0b3c708e29498a1beee2
iuri matcharashvili <119754002+iurimach@users.noreply.github.com>
2024-02-15 01:44:08 +0400

    change send function

--- smsapp.js
+++ smsapp.js
@@ -1,5 +1,6 @@
 
 
+
 // აქ ვიწყებ  დატას წამოღებას
 async function fetchData(url) {
     try {
@@ -35,7 +36,7 @@ async function fetchData(url) {
         
       console.log("Result:", result);
       if(result.length>0){ // თუ მოძებნილი ინფორმცია 0 ზე მეტია მხოლოდ მაშინ გააგზავნე მეილი
-        // SendMail();  // ამ კომენტრს ჩახსნი და დაიწყებს გაგზავნას
+         //SendMail();    // ამ კომენტრს ჩახსნი და დაიწყებს გაგზავნას ავტომატრად------------------------------------------
         }
     } catch (error) {
       console.error("Error:", error);
@@ -53,23 +54,51 @@ var message1=document.getElementById("message")
 message1.textContent=result //ამას ვწერ მეორედ რადგან ზედა დაწერილს თუ წაშლი  რესულტი იშლება,ამას თუ წაშლი ტაიპერორია 
 
 function SendMail(){
-    
+  
     var params={
         
         message:message1.textContent
     }
     // აქ ემილის პარმეტრბი წერია , email.js მა რაც მომანიჭა იდ 
     emailjs.send("service-matchara_18", "template_0843m4r",params).then(function(res){
-        alert("seccess!" + res.status);
+        
+  
     })
    
 
 }
-//აქ ვარეფლეშებ  ვებ გვერდს ყოველ 15 წუთში რადგან სულ ახლ ახლი ინფორმცია წამოღოს ხოლმე
-function refreshPage() {
-    location.reload();
-    console.log("darefleshda brat")
+//აქ ვარეფლეშებ  ვებ გვერდს შემდეგი ლოგიკით, თუ შედეგი გვაქ, მაშინ რეფლეში დაიგვიანე დიდი ხნით-ბევრი სმს რო არ გზავნოს, ვიპოვე ვიპოვეო
+// როცა რეზლტი აქვს შედის პირველ if ში - აგზვნის სმს, და ისვნებს რეფლში დიდი ხნით, რას გაუწერ
+// თუ რესალტი არ გვაქ, ვერ ვიპოვე შედეგი მაშინ რეფლეში სწრაფად გააკეთე საიტის, სანმ არ იპოვი,სანმ სერვზე არ დაიდება
+
+
+async function reloadproject(){ // ასინქ დაელოდოს, რესულტის განლიზებას,
+  result = await fetchData(url);  //ამის პასუხს უცდის ფუნქცია
+  
+  if(result.length>0){              // თუ იპოვა შედეგი რასც ვეძებ გაეშვბა ეს ტანი
+    var refreshInterval = 20 * 60 * 60 * 1000;  //= 20 hours , შეცვლე თუ სწრფი რეფლში გინდა
+    setInterval(refreshPage, refreshInterval);
+            
+    function refreshPage() { // ეს აეკეთებს რეფლეშს, 
+      location.reload();  // ამ ლოკაცის ჩატვირთვას აკეთებს თუმცა ინტერვლის  ეს ფუნქცია იღებს ზემოდან
+     
+    }
+    console.log("ipova daa da gazarda reflshis dro")
+
+  }
+  else{
+    var refreshInterval = 10 * 60 * 1000; // =10 min, თუ გინდა უფრო სწრფად დარეფლშდეს ეს პარმტრი შეცვლე
+    setInterval(refreshPage, refreshInterval);
+         
+    function refreshPage() {
+      location.reload();
+      
+    }
+    console.log("ver ipova da swrafad darefleshda")
+  }
+
+
 }
 
-const refreshInterval = 1000*90; // თუ გინდა უფრო სწრფად დარეფლშდეს ეს პარმტრი შეცვლე
-setInterval(refreshPage, refreshInterval);
+reloadproject(); //აქ ვიძახებ გვერდის რეფლეშ ფუნქციას
+
